c4a7bd58ca9caa85b1c67f9bea231b825a61fbf4
Kyle <imonfyre@gmail.com>
2021-01-10 20:08:06 -0500

    Add project files.

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using DailySpellsAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DailySpellsAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DailySpellsController : ControllerBase
+    {
+        Character[] characters = {new Character("Drizzt", "Drow", new CharacterClass[] { new CharacterClass("Barbarian", 3), new CharacterClass("Fighter", 11), new CharacterClass("Ranger", 7) }, new CharacterStatistics(13, 20, 15, 17, 17, 15)),
+            new Character("Wulfgar", "Human", new CharacterClass[] { new CharacterClass("Barbarian", 7), new CharacterClass("Fighter", 4) }, new CharacterStatistics(20, 15, 17, 13, 12, 13)),
+            new Character("Regis", "Halfing", new CharacterClass[] { new CharacterClass("Rogue", 7) }, new CharacterStatistics(10, 18, 15, 13, 15, 15)),
+            new Character("Bruenor", "Dwarf", new CharacterClass[] { new CharacterClass("Fighter", 14) }, new CharacterStatistics(18, 14, 22, 15, 13, 13)) };
+
+        HttpRequestMessage request = new HttpRequestMessage();
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Character> GetCharacters()
+        {
+            return characters;
+        }
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Character GetCharacter(int id)
+        {
+            return characters[id];
+        }
+        [HttpGet]
+        public ActionResult GetMenu()
+        {
+            return new OkObjectResult(characters.Select((x, i) => new { index = i, item = x.name }));
+        }
+       
+    }
+}
---
+++ Model/Character.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailySpellsAPI.Model
+{
+    public class Character
+    {
+        public Character() { }
+
+        public Character(string CharacterName)
+        {
+            name = CharacterName;
+        }
+
+        public Character(string CharacterName, string Race) : this(CharacterName)
+        {
+            race = Race;
+        }
+
+        public Character(string CharacterName, string Race, CharacterClass[] characterClasses, CharacterStatistics stats) : this(CharacterName, Race)
+        {
+            classes = characterClasses;
+            characterStats = stats;
+        }
+        public string name { get; set; }
+        public string race { get; set; }
+        public CharacterClass[] classes { get; set; }
+        public CharacterStatistics characterStats { get; set; }
+    }
+
+
+    public class CharacterClass
+    {
+        public CharacterClass() { }
+
+        public CharacterClass(string className, int classLevel)
+        {
+            name = className;
+            level = classLevel;
+        }
+        public string name { get; set; }
+        public int level { get; set; }
+    }
+
+    public class CharacterStatistics
+    {
+
+        public CharacterStatistics() { }
+
+        public CharacterStatistics(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+        {
+            Str = strength;
+            Dex = dexterity;
+            Con = constitution;
+            Int = intelligence;
+            Wis = wisdom;
+            Cha = charisma;
+        }
+        public int Str { get; set; }
+        public int Dex { get; set; }
+        public int Con { get; set; }
+        public int Int { get; set; }
+        public int Wis { get; set; }
+        public int Cha { get; set; }
+    }
+}
