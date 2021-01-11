e805b4c4ac7f8af3854802357a3c99b187ddbf1b
Kyle <imonfyre@gmail.com>
2021-01-11 10:24:32 -0500

    removed unnecessary usings

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,11 +1,8 @@
 ﻿using DailySpellsAPI.Model;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Web;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
